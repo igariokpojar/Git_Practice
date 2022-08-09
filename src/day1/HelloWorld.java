0c660f35651eacde750b413c717e6c583b80b7a0
Igariok <igariokpojar@yahoo.com>
2022-08-09 14:51:01 -0700

    Hello World.java is changed

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -5,5 +5,9 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello People");
+
+        // I m in vacation
+
+        // I live to travel
     }
 }
